bddc12a067f867862167f06b47e0976d52db1ae3
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-14 16:17:03 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,2 +1,13 @@
 // Code ThemedDecoration Component Here
 import React from 'react';
+
+export default class Invitation extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-header">{this.props.title}</div>
+        <div className="panel-body">{this.props.children}</div>
+      </div>
+    );
+  }
+}
